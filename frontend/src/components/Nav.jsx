ba8e4536b37da09621f4f16cb769705c5f0f390f
candyP <2689825705@qq.com>
2024-08-12 17:55:14 +0800

    debug

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,12 +1,12 @@
-const Nav = () => {
-  return (
-    <>
-      <div className="p-1 flex flex-col space-y-4">
-        <button>面板</button>
-        <button>视图</button>
-      </div>
-    </>
-  );
-};
+// const Nav = () => {
+//   return (
+//     <>
+//       <div className="p-1 flex flex-col space-y-4">
+//         <button>面板</button>
+//         <button>视图</button>
+//       </div>
+//     </>
+//   );
+// };
 
-export default Nav;
+// export default Nav;
