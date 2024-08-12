c212cc8ca3a3c96de263154509ab9fec9b6320d1
candyP <2689825705@qq.com>
2024-08-12 18:48:35 +0800

    test tobedone

--- backend/web-backend/test/controller/project.test.ts
+++ backend/web-backend/test/controller/project.test.ts
@@ -1,47 +1,47 @@
-import { createApp, close, createHttpRequest } from '@midwayjs/mock';
-import { Framework } from '@midwayjs/koa';
+// import { createApp, close, createHttpRequest } from '@midwayjs/mock';
+// import { Framework } from '@midwayjs/koa';
 
-describe('ProjectController', () => {
-  let app;
-  beforeAll(async () => {
-    app = await createApp<Framework>();
-  });
+// describe('ProjectController', async () => {
+//   let app;
+//   beforeAll(async () => {
+//     app = await createApp<Framework>();
+//   });
 
-  afterAll(async () => {
-    await close(app);
-  });   
+//   afterAll(async () => {
+//     await close(app);
+//   });   
 
-  it('should write a project', async () => {
-    const project = {};
-    const response = await createHttpRequest(app)
-      .post('/project/write')
-      .send(project);
+//   it('should write a project', async () => {
+//     const project = {};
+//     const response = await createHttpRequest(app)
+//       .post('/project/write')
+//       .send(project);
 
-    // expect(response.status).toBe(200);
-    // todo
-  });
+//     expect(response.status).toBe(200);
+//     // todo
+//   });
 
-  it('should read projects', async () => {
-    const response = await createHttpRequest(app).get('/project/read');
+//   it('should read projects', async () => {
+//     const response = await createHttpRequest(app).get('/project/read');
 
-    // expect(response.status).toBe(200);
-  });
+//     expect(response.status).toBe(200);
+//   });
 
-  it('should delete a project', async () => {
-    const projectMessage = { index: 1 };
-    const response = await createHttpRequest(app)
-      .post('/project/delete')
-      .send(projectMessage);
+//   it('should delete a project', async () => {
+//     const projectMessage = { index: 1 };
+//     const response = await createHttpRequest(app)
+//       .post('/project/delete')
+//       .send(projectMessage);
 
-    // expect(response.status).toBe(200);
-  });
+//     expect(response.status).toBe(200);
+//   });
 
-  it('should rename a project', async () => {
-    const projectMessage = { index: 1, newName: 'New Project Name' };
-    const response = await createHttpRequest(app)
-      .post('/project/rename')
-      .send(projectMessage);
+//   it('should rename a project', async () => {
+//     const projectMessage = { index: 1, newName: 'New Project Name' };
+//     const response = await createHttpRequest(app)
+//       .post('/project/rename')
+//       .send(projectMessage);
 
-    // expect(response.status).toBe(200);
-  });
-});
+//     expect(response.status).toBe(200);
+//   });
+// });
