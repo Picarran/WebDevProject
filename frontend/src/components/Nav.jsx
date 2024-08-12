aea53fa792f6cd8bf50b0ca71efa1ac62dae2b5a
candyP <2689825705@qq.com>
2024-08-12 17:54:08 +0800

    debug

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+  return (
+    <>
+      <div className="p-1 flex flex-col space-y-4">
+        <button>面板</button>
+        <button>视图</button>
+      </div>
+    </>
+  );
+};
+
+export default Nav;
