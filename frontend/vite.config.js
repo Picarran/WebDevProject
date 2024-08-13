4e6cd8abf054ed0a3e227da61f51b4643222534e
candyP <2689825705@qq.com>
2024-08-14 03:31:19 +0800

    1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/WebDevProject/",   //for github pages
   plugins: [react()],
   build: {
     outDir: "dist", // 打包后的输出目录
